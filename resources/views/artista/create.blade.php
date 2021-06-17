45a8243cad24c42c864cef3b221d6f9f2bd2e9bf
roberss11 <robertoss11d@gmail.com>
2021-06-17 15:20:47 +0200

    cambios

--- resources/views/artista/create.blade.php
+++ resources/views/artista/create.blade.php
@@ -2,5 +2,29 @@
 formulario creacion artistas
 
 <form action="" method="post" enctype="multipart/form-data" >
-<input type="text" name="Nombre">
+
+<label for="Nombre"> Nombre </label>
+<input type="text" name="Nombre" id="Nombre">
+<br>
+<label for="Apellido"> Apellido </label>
+<input type="text" name="Apellido" id="Apellido">
+<br>
+<label for="Edad"> Edad </label>
+<input type="number" name="Edad" id="Edad">
+<br>
+<label for="Nacionalidad"> Nacionalidad </label>
+<input type="text" name="Nacionalidad" id="Nacionalidad">
+<br>
+<label for="Tipo"> Tipo </label>
+<input type="text" name="Tipo" id="Tipo">
+<br>
+<label for="Género musical"> Género musical </label>
+<input type="text" name="Género musical" id="Género musical">
+<br>
+<label for="Foto"> Foto </label>
+<input type="file" name="Foto" id="Foto">
+<br>
+<label for="Enviar"> Enviar </label>
+<input type="submit" name="Enviar" id="Enviar">
+<br>
 </form>
